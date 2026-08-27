affc694267e8664a3a98b02d1dd9556e68c28a95
daciple-matthew <daciplerosemateo@gmail.com>
2026-08-27 08:08:27 +0800

    added queries for borrow and return

--- lab3.sql
+++ lab3.sql
@@ -17,10 +17,26 @@ CREATE TABLE borrow (
 );
 
 SELECT br.borrow_id, s.student_id,
-    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, s.student_course,
-    b.book_title, b.book_author, b.book_category,
-    br.borrow_date 
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, 
+    b.book_title, b.book_author, b.book_categ,
+    br.borrow_date, br.borrow_return_date
 FROM borrow br
     JOIN students s ON br.student_id = s.student_id
     JOIN books b ON br.book_id = b.book_id
+    WHERE br.borrow_return_date IS NULL
 ORDER BY br.borrow_date DESC;
+
+-- alter borrow_return_date to allow NULL values and set default to NULL
+ALTER TABLE borrow
+MODIFY borrow_return_date TIMESTAMP NULL DEFAULT NULL;
+
+UPDATE borrow
+SET borrow_return_date = NULL
+WHERE borrow_return_date = '2026-08-25 08:31:40';
+
+-- Return a book by updating the borrow_return_date to the current timestamp   
+
+UPDATE borrow
+SET borrow_return_date = CURRENT_TIMESTAMP
+-- Change value of borrow_return_date to NULL for a specific borrow_id
+WHERE borrow_id = 1 AND borrow_return_date IS NULL;
