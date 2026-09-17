270ff97784a89300e63a4e01d248677c6f9f0f65
daciple-matthew <daciplerosemateo@gmail.com>
2026-09-17 08:05:52 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+// database connection
+$host = 'localhost';
+$db = 'library_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host; dbname=$db; charset=$charset";
+
+$options = [
+PDO::ATTR_ERR => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEAFULT_FETCH_MODE => PDO::FETCH_ASSOC,
+PDO::ATTR_EMULATE_PREPARES => false,
+
+
+]
+try{
+    $pdo = new PDO($dsn,$user,$pass,$options);
+    echo 'connection successful';
+
+}catch(PDOException $e) {
+    die("Database connection failed" . $e->getMessage());
+}
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
